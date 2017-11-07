21393875e6042fb1d43619be5b9f785c53ade6dd
wallace.prevot <wallace.prevot@RJOD212243.GOV>
2017-11-07 11:49:30 -0200

    inicial

---
+++ src/tutorialWeb/OlaBean.java
@@ -0,0 +1,25 @@
+package tutorialWeb;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+public class OlaBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private String nome;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+
+}
